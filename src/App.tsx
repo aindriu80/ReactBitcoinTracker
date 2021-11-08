19abe06026924cfd7e1c8a1fe42c93c1d60d51b8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-11-08 16:28:50 +0000

    Base currency selector && styling

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,47 @@ const getBCData = async (): Promise<Currencies> =>
 const INTERVAL_TIME = 30000 // 30 seconds
 
 const App = () => {
+  const [currency, setCurrency] = useState('USD')
   const { data, isLoading, error, refetch } = useQuery<Currencies>(
     'bc-data',
     getBCData
   )
-  return <div className="App">Tosaigh</div>
+  // console.log('refeching data')
+  // console.log(data)
+
+  const handleCurrencyChange = (e: any) => {
+    setCurrency(e.currentTarget.value)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(refetch, INTERVAL_TIME)
+    return () => clearInterval(interval)
+  }, [refetch])
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>We have an error Captain...</div>
+
+  return (
+    <Wrapper>
+      <>
+        <h2>Bitcoin Prices</h2>
+        <select value={currency} onChange={handleCurrencyChange}>
+          {data &&
+            Object.keys(data).map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+        </select>
+        <div>
+          <h2>
+            {data && data[currency].symbol}&nbsp;
+            {data && data[currency].last}
+          </h2>
+        </div>
+      </>
+    </Wrapper>
+  )
 }
 
 export default App

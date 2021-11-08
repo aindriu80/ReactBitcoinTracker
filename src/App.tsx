fa592f5aa8d901a449d5f0c2cb1871c5e1ab137e
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-11-08 15:31:39 +0000

    Connecting to Internet & retrieving current price

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from 'react'
+import { useQuery } from 'react-query'
+
+//Styles
+import { Wrapper } from './styles/App.styles'
+
+type BitcoinData = {
+  '15m': number
+  buy: number
+  last: number
+  sell: number
+  symbol: string
+}
+
+type Currencies = {
+  [key: string]: BitcoinData
+}
+
+const getBCData = async (): Promise<Currencies> =>
+  await (await fetch('https://blockchain.info/ticker')).json()
+
+const INTERVAL_TIME = 30000 // 30 seconds
+
 const App = () => {
+  const { data, isLoading, error, refetch } = useQuery<Currencies>(
+    'bc-data',
+    getBCData
+  )
   return <div className="App">Tosaigh</div>
 }
 
